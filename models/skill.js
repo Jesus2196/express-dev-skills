6e32a2487e5e45557a0b62afedd280d6eced78a4
Jesus <Jesuspineda9512@gmail.com>
2021-10-08 19:08:35 -0700

    Functioning add/edit skills

--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,35 @@
 const skills = [
-    { id: 314, skill: 'HTML', learning: true },
-    { id: 714, skill: 'C++', learning: false },
-    { id: 365, skill: 'JavaScript', learning: true },
-    { id: 007, skill: 'Python', learning: false }
+    { id: 314, skill: 'HTML', done: true },
+    { id: 714, skill: 'C++', done: false },
+    { id: 365, skill: 'JavaScript', done: true },
+    { id: 007, skill: 'Python', done: false }
 ];
 
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 };
+
+function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+}
 
 function getOne(id) {
     id = parseInt(id);
